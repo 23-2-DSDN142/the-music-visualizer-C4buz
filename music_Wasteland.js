0f27d3e2a8f9030f4920b383462d26ee3d17e503
C4buz <140349703+C4buz@users.noreply.github.com>
2023-09-27 17:57:49 +1300

    bugs

--- music_Wasteland.js
+++ music_Wasteland.js
@@ -6,11 +6,8 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 //To do List
 //Leaves growing over time
 //Set Up
-//Italic on 'Wasteland, baby!'
-//Make last words fade
-//Dont want 'thats it' to read
 //Can we load images?
-// fade to black
+
 
 //Loading image sequences and assets is in NUKU
 //animate the alpha value 
@@ -24,84 +21,63 @@ textFont('TIMES'); // please use CSS safe fonts
 rectMode(CENTER)
 textSize(30);
 
-let EllipseSize = map (bass,0,100,70,300) // Restricst size, bass in here means not needed- does this affect all cirlces?
-  
-let blue = color(98, 121, 158)
-let green = color (82, 125, 88)
-let LerpMap = map (vocal,0,100,0,1)
-let middleColor =lerpColor (blue,green,LerpMap) //The greater the vocals the more green!
+//how to limit firefly size?
+//Want fireflys to only turn on once he starts singing
 
-let Leafx= 8
-let Leafy= 10
-let LeafOutline = 0.5;
-let leaf_colour= color(71, 110, 84);
-let leaf_stroke_colour = color(52, 79, 61);
 
-//Leaf 
-fill (leaf_colour)
-stroke(leaf_stroke_colour)
-strokeWeight(LeafOutline)
-beginShape ();
-curveVertex (Leafx+38,Leafy+190);
-curveVertex (Leafx+38,Leafy+190);
-curveVertex (Leafx+27,Leafy+170);
-curveVertex (Leafx+31,Leafy+150);
-curveVertex (Leafx+31,Leafy+150);
-endShape (CLOSE);
-  
-beginShape ();
-curveVertex (Leafx+38,Leafy+190);
-curveVertex (Leafx+38,Leafy+190);
-curveVertex (Leafx+42,Leafy+170);
-curveVertex (Leafx+31,Leafy+150);
-curveVertex (Leafx+31,Leafy+150);
-endShape (CLOSE);
+//Firefly
+let firefly_x = 0
+let firefly_y = 0
+let firefly_w = 0
+let firefly_h = 0
 
-  
-//Middle Circle 
-fill (middleColor)
+//Glow
+let glowsize = 15
+let fireflysize = map (bass,0,100,0,glowsize)
+
+let yellow = color(222, 171, 78)
+let bright = color (255, 248, 199)
+let LerpMap = map (bass,0,200,0,1)
+let fireflyfill = lerpColor (yellow,bright,LerpMap)
+
+fill (fireflyfill)
 strokeWeight (0)
-ellipse (width/2, height/6*4,300+vocal,300+vocal)
+ellipse (firefly_x+92,firefly_y+96,fireflysize+4,fireflysize)
 
-//Lyrics 
-fill(255);
-textAlign(CENTER);
-text(words, width/2, height/2);
+//firefly body
+fill (69, 55, 48)
+beginShape ();
+curveVertex (124,90);
+curveVertex (95,92); 
+curveVertex (95,99);
+curveVertex (120,100);
+endShape (CLOSE); 
+
+ellipse (firefly_x+125,firefly_y+93,firefly_w+8,firefly_h+6) // neck
+ellipse (firefly_x+131,firefly_y+94,firefly_w+6,firefly_h+7) // head
+
+//firefly body outline
+noFill()
+strokeWeight (1)
+stroke (36, 35, 32)
+beginShape ();
+curveVertex (124,90);
+curveVertex (124,90);
+curveVertex (95,92); 
+curveVertex (95,99);
+curveVertex (120,100);
+curveVertex (124,95);
+curveVertex (124,95);
+endShape (); 
+
+ellipse (firefly_x+125,firefly_y+93,firefly_w+8,firefly_h+6) // neck
+ellipse (firefly_x+131,firefly_y+94,firefly_w+6,firefly_h+7) // head
+
+
+
+
 
   
 
-  //  let bar_spacing = height / 10;
-  //  let bar_height = width / 12;
-  //  let bar_pos_x = width / 2;
  
-
-  //  // vocal bar is red
-  //  fill(200, 0, 0);
-  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  //  fill(0);
-  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-  //  // drum bar is green
-  //  fill(0, 200, 0);
-  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  //  fill(0);
-  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-  //  // bass bar is blue
-  //  fill(50, 50, 240);
-  //  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  //  fill(0);
-  //  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-  //  // other bar is white
-  //  fill(200, 200, 200);
-  //  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  //  fill(0);
-  //  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-  //  fill(255, 255, 0);
- 
-  //  // display "words"
-  //  textAlign(CENTER);
-  //  textSize(vocal);///Louder vocals= biggere text
-  //  text(words, width/2, height/3);
 }
