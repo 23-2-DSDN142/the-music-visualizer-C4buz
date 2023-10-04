06127135fea08ee7def3416d8a22b3de242f6c01
C4buz <140349703+C4buz@users.noreply.github.com>
2023-10-04 15:03:46 +1300

    s

--- music_Wasteland.js
+++ music_Wasteland.js
@@ -1,48 +1,48 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
+let firstRun = true
+let glowImg;
+
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
+
+
 console.log (song.currentTime())
-
-//how to get grass growing over time
-
-//how to animate the alpha value 
 
 background(49, 74, 87)
 ellipseMode (CENTER)
 rectMode(CENTER)
 textFont('TIMES'); // please use CSS safe fonts
 textSize(30);
-draw_images(vocal, drum, bass, other)
-
 
 
 //Gradient Background 
 
-let skyMap= map(song.currentTime(), 0,song.duration(), 0, 1);
+// let skyMap= map(song.currentTime(), 0,song.duration(), 0, 1);
 
-let duskTop = color (77, 101, 138)
-let duskBottom = color (151, 194, 175)
+// let duskTop = color (77, 101, 138)
+// let duskBottom = color (151, 194, 175)
 
-let nightTop = color (5, 14, 36)
-let nightBottom = color (41, 48, 64)
+// let nightTop = color (5, 14, 36)
+// let nightBottom = color (41, 48, 64)
 
-let top = lerpColor (duskTop, nightTop, skyMap)
-let bottom = lerpColor (duskBottom, nightBottom, skyMap)
+// let top = lerpColor (duskTop, nightTop, skyMap)
+// let bottom = lerpColor (duskBottom, nightBottom, skyMap)
 
 
-for (let i = 0; i<height; i++){
+// for (let i = 0; i<height; i++){
 
-    let strokeMap= map(i,0,height, 0, 1);
-    let backgroundChange = lerpColor (bottom,top,strokeMap)
+//     let strokeMap= map(i,0,height, 0, 1);
+//     let backgroundChange = lerpColor (bottom,top,strokeMap)
    
-    stroke(backgroundChange);
-    strokeWeight(1.5)
-    noFill()
-    // line(0, i, width, i)
-    ellipse(width/2, height, width + i, 500 + i*2)
+//     stroke(backgroundChange);
+//     strokeWeight(1.5)
+//     noFill()
+//     line(0, i, width, i)
+//     ellipse(width/2, height, width + i, 500 + i*2)
 
-}
+// }
+
 
 
 // Vocal moon
@@ -57,28 +57,42 @@ fill (255)
 ellipse (width/2,height/2,100+vocal-30,100+vocal-30);
   }
    
-
-
-
 //Firefly
+
 let firefly_x = 0
 let firefly_y = 0
 let firefly_w = 0
 let firefly_h = 0
 
-
 //Glow
-let glowsize = 30
+
+if (firstRun) {
+glowImg = loadImage ('glow.png');
+firstRun = false
+}
+push()
+image(glowImg,100,100)
+pop()
+
+
+let glowsize = 20
+let outerglowsize = 60
 let fireflyMap = map (drum,0,100,0,glowsize)
+let outerfireflyMap = map (drum,0,100,0,outerglowsize)
 
 let yellow = color(222, 171, 78)
-let bright = color (255, 248, 199)
+let bright = color (255, 255, 255)
 let LerpMap = map (drum,0,200,0,1)
 let fireflyfill = lerpColor (yellow,bright,LerpMap)
 
-fill (fireflyfill)
 strokeWeight (0)
-ellipse (firefly_x+92,firefly_y+96,fireflyMap+4,fireflyMap)
+
+fill (255, 179, 87,30)
+ellipse (firefly_x+92,firefly_y+96,outerfireflyMap+20,outerfireflyMap+20)
+
+
+fill (fireflyfill)
+ellipse (firefly_x+92,firefly_y+96,fireflyMap+4,fireflyMap+4)
 
 
 // let glowArray = [color(),color(),color(),color()] //create a firefly glow glow, want the alpha value to change
@@ -127,27 +141,7 @@ line (101,100,99,104)
 }
 
 
-let firstRun = true
-let Grass = [];
 
-function draw_images(vocal,drum,bass,other){
-if (firstRun) {
-  Grass.push(loadImage('grass_0.png'));
-  Grass.push(loadImage('grass_1.png'));
-  Grass.push(loadImage('grass_2.png'));
-
-  firstRun = false
-}
-
-var VocalFrame = int (map(other,0,100,0,3));
-console.log(VocalFrame);
-push();
-scale(0.15);
-image(Grass[VocalFrame],width/2,height/2)
-pop();
-
-
-}
 
 
 
